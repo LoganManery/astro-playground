05014a448e2ebc7192375932c060accab97ec0a6
LoganManery <125079387+LoganManery@users.noreply.github.com>
2023-09-28 14:08:36 -0400

    added google font

--- tutorial/tailwind.config.js
+++ tutorial/tailwind.config.js
@@ -2,6 +2,25 @@
 export default {
   content: ["./src/**/*.astro"],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      base: '#EAAC57',
+      primaryr: '#2B359E',
+      primaryl: '#63EBD6',
+      primaryrr: '#4B59EB',
+      primaryll: '#4A9E91',
+    },
+    fontFamily: {
+      sans: ['Inter var', 'sans-serif'],
+      impact: ['Impact', 'fantasy'],
+      emoji: ['emoji'],
+      roboto: ['Roboto', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
