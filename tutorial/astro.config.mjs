ee6d3ac410d6c4ffc214634967d42356e0778ce9
LoganManery <125079387+LoganManery@users.noreply.github.com>
2023-10-05 23:12:14 -0400

    added functionality to the site and prettied it up

--- tutorial/astro.config.mjs
+++ tutorial/astro.config.mjs
@@ -1,11 +1,22 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import preact from "@astrojs/preact";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   outDir: './build',
-  integrations: [tailwind(), preact()],
+  renderers: ['@astro/types/renderer-mdx'],
+  integrations: [
+    tailwind(), 
+    preact(), 
+    mdx(
+    {
+      syntaxHighlight: 'shiki',
+      shikiConfig: { theme: 'dracula' },
+      remarkRehype: { footnoteLabel: 'Footnotes' },
+      gfm: false,
+    }
+  )],
   site: "https://example.com"
 });
--- tutorial/src/components/Greeting.jsx
+++ tutorial/src/components/Greeting.jsx
@@ -1,14 +1,11 @@
 import { useState } from 'preact/hooks';
 
 export default function Greeting({messages}) {
-  const randomMessage = () => messages[(Math.floor(Math.random() * messages.length))];
-
   const [greeting, setGreeting] = useState(messages[0]);
 
   return (
-    <div>
-      <h3>{greeting}! Thank you for visiting!</h3>
-      <button onClick={() => setGreeting(randomMessage())}>New Greeting</button>
+    <div class="w-full">
+      <p class="font-pixelify text-2xl text-center cursor-pointer" onClick={() => setGreeting(() => messages[(Math.floor(Math.random() * messages.length))])}>{greeting}! Thank you for visiting!</p>
     </div>
   )
 }
