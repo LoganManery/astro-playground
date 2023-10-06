44da2f005b9bdb31bf6c3aaee716d39403dc0659
LoganManery <125079387+LoganManery@users.noreply.github.com>
2023-10-06 12:10:14 -0400

    added more functionality and view transitions

--- tutorial/src/scripts/menu.js
+++ tutorial/src/scripts/menu.js
@@ -2,3 +2,8 @@ document.getElementById('menuButton').addEventListener('click', function () {
   var menu = document.getElementById('menu');
   menu.classList.toggle('hidden');
 });
+
+document.getElementById('menu').addEventListener('mouseleave', function () {
+  var menu = document.getElementById('menu');
+  menu.classList.toggle('hidden');
+});
